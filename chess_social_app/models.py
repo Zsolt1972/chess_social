3fae49dc63587d3eabf7f235a8f73da0c30f7c9d
Szabó Zsolt <zsobe31@gmail.com>
2021-12-28 14:13:23 +0100

    first change

--- chess_social_app/models.py
+++ chess_social_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+'''
+rnbqkbnrppppppppxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxPPPPPPPPRNBQKBNR
+'''
+
+class Position(models.Model):
+    positions_of_figures = models.CharField(max_length=64, default='rnbqkbnrppppppppxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxPPPPPPPPRNBQKBNR')
+    step_count = models.DecimalField(max_digits=10, decimal_places=0, default='')
+    player_name = models.CharField(max_length=20, default='')
+    white_or_black = models.CharField(max_length=5, default='New')
+    date_of_step = models.DateTimeField(auto_now_add=True)
--- chess_social_app/views.py
+++ chess_social_app/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello django!')
+    return render(request, 'chess_social_app/index.html')
+    # return HttpResponse('Hello django!')
